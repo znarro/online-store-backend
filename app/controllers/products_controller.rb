78a7fc5e9291153887b17670d907e009c585567a
Josue Narro <josue.narro.a@gmail.com>
2021-04-11 17:44:25 -0500

    Add category_id param

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,18 @@
 class ProductsController < ApplicationController
   def index
-    @products = filter(Product.all)
+    if params[:category_id]
+      category = Category.find(params[:category_id])
+      @products = category.products
+    else
+      @products = filter_by_name(Product.all)
+    end
+
     render json: @products
   end
 
   private
 
-  def filter(products)
+  def filter_by_name(products)
     products.where('name LIKE ?', "%#{params[:q]}%").order(category: :asc)
   end
 end
